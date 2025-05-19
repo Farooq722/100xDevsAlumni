cf87f680a446c06a328f22bb915ef20e8760b61d
B FAROOQ <bfarooq2022@gmail.com>
2025-05-20 00:00:08 +0530

    be code modified at auth route

--- apps/backend/src/middleware/alumniMiddleware.ts
+++ apps/backend/src/middleware/alumniMiddleware.ts
@@ -6,8 +6,10 @@ export const alumniMiddleware = async (
   res: Response,
   next: NextFunction,
 ) => {
-  const header = req.headers["authorization"];
-  const token = header?.split(" ")[1];
+  const authHeader = req.headers["authorization"];
+  const token = authHeader
+    ? authHeader.split(" ")[1] // Bearer token
+    : req.cookies.token;
 
   if (!token) {
     res.status(404).json({ msg: "Invalid token" });
