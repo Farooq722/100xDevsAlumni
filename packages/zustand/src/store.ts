132612ba547b007d9736cd1dd6516c8ae7e30c23
B FAROOQ <bfarooq2022@gmail.com>
2025-05-25 17:04:30 +0530

    env added to state management

--- packages/zustand/src/store.ts
+++ packages/zustand/src/store.ts
@@ -24,60 +24,12 @@ export const useStore = create<UserStore>()(
   ),
 );
 
-// type UserStore = {
-//   user: boolean;
-//   loader: boolean;
-//   token: string | null;
-//   setUser: (user: boolean) => void;
-//   setLoader: (status: boolean) => void;
-//   setToken: (token: string) => void;
-//   logout: () => void;
-// };
-
-interface User {
-  id: string;
-  name: string;
-  username: string;
-  avatar: string;
-  avatarId: string;
-  bio: string;
-  role: string;
-}
-
-type selfData1 = {
-  selfData: User | null;
+type allAlumniData = {
   allAlumniData: Array<any>;
-  setSelfData: (user: User) => void;
   setAllAlumniData: (allAlumniData: Array<any>) => void;
 };
 
-// export const useStore = create<UserStore>()(
-//   persist(
-//     (set) => ({
-//       user: false,
-//       loader: false,
-//       token: null,
-
-//       setUser: (user) => set({ user }),
-//       setLoader: (status) => set({ loader: status }),
-//       setToken: (token) => {
-//         localStorage.setItem("token", token);
-//         set({ token });
-//       },
-//       logout: () => {
-//         localStorage.removeItem("token");
-//         set({ token: null });
-//       },
-//     }),
-//     {
-//       name: "user-auth", // This is the localStorage key
-//     },
-//   ),
-// );
-
-export const useData = create<selfData1>()((set) => ({
-  selfData: null,
+export const useData = create<allAlumniData>()((set) => ({
   allAlumniData: [],
-  setSelfData: (data) => set({ selfData: data }),
   setAllAlumniData: (allAlumniData) => set({ allAlumniData }),
 }));
